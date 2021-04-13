431fa9b6529e55188fdc89ebdf070a7418d69864
Sergei Liddell <liddell.s@pm.me>
2021-04-12 23:05:56 -0600

    Creates basic navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Button from './components/Button'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
-      <Button />
+      <Navbar />
     </div>
   );
 }
