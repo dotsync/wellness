18a8fc2015992df86c222d2e201703f8a8983781
Sergei Liddell <liddell.s@pm.me>
2021-04-12 22:41:30 -0600

    Configues useStyle hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  helloWorldStyle: {
+    fontSize: '200px'
+  }
+})
+
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
+    <div className={classes.helloWorldStyle}>
       <h1>Hello world!</h1>
     </div>
   );
