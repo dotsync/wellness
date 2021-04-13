402a82cd4cd29faef28964c59afe6b1f5d571f8f
Sergei Liddell <liddell.s@pm.me>
2021-04-13 10:00:57 -0600

    Removes comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
   root: {
