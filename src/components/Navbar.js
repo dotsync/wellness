93650b64e74636359c10a8ffb734647ce1299a32
Sergei Liddell <liddell.s@pm.me>
2021-04-13 12:46:41 -0600

    Implements Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 
+import { Paper, Grid } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    height: '20px'
   },
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  accountAvatarButton: {
+    flexGrow: 1,
+    marginLeft: 30
   },
   toolbar: {
     minHeight: 128,
@@ -29,13 +34,19 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Grid container spacing={3}>
+            <Grid item xs={2}>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={8} />
+            <Grid item xs={2}>
+              <IconButton edge="start" className={classes.accountAvatarButton} color="inherit" aria-label="account">
+                <AccountCircleIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
