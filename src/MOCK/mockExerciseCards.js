3892d458994feea389c60e5c0778b285107bd006
Sergei Liddell <liddell.s@pm.me>
2021-04-13 18:47:00 -0600

    Adds ids to mock data

--- src/MOCK/mockExerciseCards.js
+++ src/MOCK/mockExerciseCards.js
@@ -1,47 +1,55 @@
 const mockExerciseCards = [
   {
+    id: 1,
     sport: 'Bike',
     type: 'Speed',
     description: 'Interval sprints to develop speed',
     image: '/static/images/cards/bike-speed.jpg'
   },
   {
+    id: 2,
     sport: 'Bike',
     type: 'Endurance',
     description: 'Long rolling hills to develop endurance',
     image: '/static/images/cards/bike-endurance-hill.jpg'
   },
   {
+    id: 3,
     sport: 'Run',
     type: 'Speed',
     description: '3 Minute sprints to develop speed',
     image: '/static/images/cards/run-speed.jpg'
   },
   {
+    id: 4,
     sport: 'Run',
     type: 'Endurance',
     description: 'Long rolling hills to develop endurance',
     image: '/static/images/cards/run-endurance-hill.jpg'
   },
   {
+    id: 5,
     sport: 'Swim',
     type: 'Speed',
     description: '100 Meter sprints to develop speed',
     image: '/static/images/cards/swim-speed.jpg'
   },
   {
+    id: 6,
     sport: 'Swim',
     type: 'Endurance',
     description: '500 Meter sustained low effort to develop endurance',
     image: '/static/images/cards/swim-endurance-openwater.jpg'
   },
   {
+    id: 7,
     sport: 'Yoga',
     type: 'Recovery',
     description: 'A light yoga routine to align and stretch the back and neck',
     image: '/static/images/cards/yoga-alignment.jpg'
   },
   {
+    id: 8,
     sport: 'Yoga',
     type: 'Triathlete',
     description: 'A 10 minute recover sequence for triathletes',
